fb0c86511913353fc56f11405d08002e70c70019
Rafael Dias <rafaeldias@MacBook-Air-de-Rafael-2.local>
2024-10-07 11:54:49 -0300

    build(config): adicionar configuração de paths para @schemas e @models

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@schemas": path.resolve(__dirname, "src/schemas"),
+      "@models": path.resolve(__dirname, "src/models"),
+    },
+  },
+});
