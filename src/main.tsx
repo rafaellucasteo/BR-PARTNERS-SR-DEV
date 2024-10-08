97896f70af27d74183cb5fc278e6f3ab1a218ff8
Rafael Dias <rafaeldias@MacBook-Air-de-Rafael-2.local>
2024-10-08 16:28:09 -0300

    feat(react-query): configurar React Query com Provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,32 @@ import {
   ThemeProvider as MUIThemeProvider,
   StyledEngineProvider,
 } from "@mui/material/styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider as StyledThemeProvider } from "styled-components";
 import App from "./App.tsx";
 import { theme } from "./styles/theme/index.ts";
 
-async function msw() {
+const queryClient = new QueryClient();
+
+async function initializeMsw() {
   const { worker } = await import("./services/mocks/browser");
   worker.start();
 }
 
-msw().then(() => {
+initializeMsw().then(() => {
   createRoot(document.getElementById("root")!).render(
-    <StyledEngineProvider injectFirst>
-      <MUIThemeProvider theme={theme}>
-        <StyledThemeProvider theme={theme}>
-          <StrictMode>
-            <App />
-          </StrictMode>
-        </StyledThemeProvider>
-      </MUIThemeProvider>
-    </StyledEngineProvider>
+    <StrictMode>
+      <StyledEngineProvider injectFirst>
+        <MUIThemeProvider theme={theme}>
+          <StyledThemeProvider theme={theme}>
+            <QueryClientProvider client={queryClient}>
+              <App />
+            </QueryClientProvider>
+          </StyledThemeProvider>
+        </MUIThemeProvider>
+      </StyledEngineProvider>
+    </StrictMode>
   );
 });
