9c6d0ddaed8aaa53a60aaa320568f32a4b432348
Rafael Dias <rafaeldias@MacBook-Air-de-Rafael-2.local>
2024-10-08 11:29:34 -0300

    feat(type): Ajusta importação de clientSchema

--- src/types/client.ts
+++ src/types/client.ts
@@ -1,4 +1,4 @@
-import { clientSchema } from "@schemas/clientSchema";
 import { z } from "zod";
+import { clientSchema } from "../schemas/clientSchema";
 
 export type Client = z.infer<typeof clientSchema>;
