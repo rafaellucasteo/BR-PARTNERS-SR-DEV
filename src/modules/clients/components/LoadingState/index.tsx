9969a89631e98e2d5443e7f2301e78e55f0dcbcf
Rafael Dias <rafaeldias@MacBook-Air-de-Rafael-2.local>
2024-10-11 08:36:17 -0300

    feat(LoadingState): adiciona componente de estado de carregamento

---
+++ src/modules/clients/components/LoadingState/index.tsx
@@ -0,0 +1,23 @@
+import { LinearProgress } from "@mui/material";
+import Box from "../../../../components/atoms/Box";
+import Card from "../../../../components/atoms/Card";
+
+interface LoadingStateProps {}
+
+export default function LoadingState({}: LoadingStateProps) {
+  return (
+    <Card
+      elevation={1}
+      sx={{
+        height: 400,
+        width: 1323,
+        display: "flex",
+        backgroundColor: "#042940",
+      }}
+    >
+      <Box sx={{ width: "100%" }}>
+        <LinearProgress />
+      </Box>
+    </Card>
+  );
+}
