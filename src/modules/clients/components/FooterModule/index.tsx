62a9cf520247c6e25a8764ee15e419660ad1684b
Rafael Dias <rafaeldias@MacBook-Air-de-Rafael-2.local>
2024-10-11 08:34:56 -0300

    feat(FooterModule): adiciona módulo de rodapé

---
+++ src/modules/clients/components/FooterModule/index.tsx
@@ -0,0 +1,21 @@
+import Box from "../../../../components/atoms/Box";
+
+interface FooterModuleProps {
+  children?: React.ReactNode;
+}
+
+export default function FooterModule({ children }: FooterModuleProps) {
+  return (
+    <Box
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 20,
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
